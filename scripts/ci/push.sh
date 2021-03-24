e07612723333a0c5e45a946bcc67fbb4803794e7
Christian Nunciato <c@nunciato.org>
2021-03-24 16:30:34 -0700

    Add GitHub workflows

---
+++ scripts/ci/push.sh
@@ -0,0 +1,15 @@
+# #!/bin/bash
+
+# set -o errexit -o pipefail
+
+# source ./scripts/ci-login.sh
+
+# ./scripts/build-site.sh
+# ./scripts/sync-and-test-bucket.sh
+# ./scripts/publish-sentry-release.sh
+
+# # Wait for in-progress jobs to complete before proceeding.
+# node ./scripts/await-in-progress.js
+
+# ./scripts/run-pulumi.sh update
+# ./scripts/make-s3-redirects.sh
--- scripts/common.sh
+++ scripts/common.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# The default theme.
-export HUGO_THEME="current"
-
 # Bundle filenames are exported for use with Hugo and theme scripts.
 export CSS_BUNDLE="static/css/styles.dev.css"
 export JS_BUNDLE="static/js/bundle.min.dev.js"
