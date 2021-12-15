65e57ac52809fe05c0d165f9a9050e48829b2dc7
Laura Santamaria <laura@nimbinatus.com>
2021-12-14 18:23:29 -0600

    revert(accident): revert accidental commit

--- scripts/content/new-learn-module.sh
+++ scripts/content/new-learn-module.sh
@@ -5,13 +5,11 @@ set -o errexit -o pipefail
 module=""
 topic=""
 
-content_root="themes/default/content/"
-
 prompt_for_module_name() {
     read -p "Module name (e.g., pulumi-101-aws): " module
 
     if [ ! -z "$module" ]; then
-        hugo new --kind learn/module "${content_root}learn/${module}"
+        hugo new --kind learn/module "learn/${module}"
         return
     fi
 
@@ -23,7 +21,7 @@ prompt_for_topic_name() {
     read -p "Topic name (e.g., basics): " topic
 
     if [ ! -z "$topic" ]; then
-        hugo new --kind learn/topic "${content_root}learn/${module}/${topic}"
+        hugo new --kind learn/topic "learn/${module}/${topic}"
         return
     fi
 
