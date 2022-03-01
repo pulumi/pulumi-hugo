581aa866d1457020f6dd1d3bcdcc3423b3878b06
Christian Nunciato <c@nunciato.org>
2022-02-28 17:22:13 -0800

    Go back to the script

--- scripts/ci/build-full-site.sh
+++ scripts/ci/build-full-site.sh
@@ -10,7 +10,8 @@ rm -rf temp-docs && mkdir temp-docs
 # Use this pulumi-hugo branch's themes/default content to run a full build of the website.
 pushd temp-docs
     git clone https://github.com/pulumi/docs.git .
-    HUGO_MODULE_REPLACEMENTS="github.com/pulumi/pulumi-hugo/themes/default -> $(pwd)/../themes/default" make ensure ensure_tools build
+    HUGO_MODULE_REPLACEMENTS="github.com/pulumi/pulumi-hugo/themes/default -> $(pwd)/../themes/default" \
+        make ensure ensure_tools build
 popd
 
 rm -rf temp-docs
