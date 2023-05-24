c62d42846ff0d4ce061e10679c5831203a8d6aa6
Sean Holung <sean.holung@gmail.com>
2023-05-24 15:38:52 -0700

    test

--- scripts/ci/ci-login.sh
+++ scripts/ci/ci-login.sh
@@ -2,6 +2,9 @@
 
 set -o errexit -o pipefail
 
+source ./scripts/ci/common.sh
+clone_docs_infrastructure
+
 echo "Selecting the ${PULUMI_STACK_NAME} stack"
 pulumi login
 pulumi -C infrastructure stack select "${PULUMI_STACK_NAME}"
