aaded0a43d9fcf1e2e864a4ab18a601d5a79370c
Christian Nunciato <chris@nunciato.org>
2023-12-06 16:18:05 -0800

    Add VPC subnets examples

--- themes/default/static/programs/awsx-vpc-azs-javascript/index.js
+++ themes/default/static/programs/awsx-vpc-azs-javascript/index.js
@@ -1,7 +1,6 @@
 "use strict";
 const awsx = require("@pulumi/awsx");
 
-// Allocate a new VPC with a custom CIDR block.
 const vpc = new awsx.ec2.Vpc("vpc", {
     numberOfAvailabilityZones: 4,
 });
